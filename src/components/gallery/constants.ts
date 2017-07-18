510a3dd92f92f4a5f9490995454e35fa06ffe2b5
Pasha <pashka.zevs@gmail.com>
2017-07-17 22:02:14 -0600

    added gallery layout.

---
+++ src/components/gallery/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by sanitizer on 7/17/17.
+ */
+
+export const BASE_GALLERY_DIR: string = "/pics/gallery/";
--- src/components/gallery/gallery.ts
+++ src/components/gallery/gallery.ts
@@ -1,13 +1,24 @@
+import {Picture} from "../../resources/model/picture";
+import {BASE_GALLERY_DIR} from "./constants";
 /**
  * Created by sanitizer on 7/14/17.
  */
 
 export class Gallery {
 
-    data: string;
+    pics: Array<Picture>;
 
     constructor() {
-        this.data = "hello this is gallery";
+        this.pics = [new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Some Pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Some Pic, but another"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Even better pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Amazing pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Hahaha pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Pic pic pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "A pic is a pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Well you know pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Another pic"),
+                     new Picture(BASE_GALLERY_DIR + "/pic.jpg", "Another pic")];
     }
 
 }

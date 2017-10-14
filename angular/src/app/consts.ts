85f2c9becaca5d03864524dff109879b473cb641
Pasha <pashka.zevs@gmail.com>
2017-10-13 20:10:23 -0600

    fixed the position where to put static assets. added consts file just to reference the assests correctly later on

---
+++ angular/src/app/consts.ts
@@ -0,0 +1,1 @@
+export const BASE_ASSETS_PATH: string = "/assets/";
--- angular/src/app/modules/services/components/card/card.component.ts
+++ angular/src/app/modules/services/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Picture} from "../../../../components/common/model/picture";
+import {BASE_ASSETS_PATH} from "../../../../consts";
 
 @Component({
   selector: 'app-card',
@@ -15,7 +16,7 @@ export class CardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.pic = new Picture("../../../images/misc/sign.jpg");
+    this.pic = new Picture(BASE_ASSETS_PATH + "images/misc/sign.jpg");
     this.header = "A card";
     this.text = "Testing the text as is. Testing the text as is.\nTesting the text as is.Testing the text as is.Testing the text as is.Testing the text as isTesting the text as is. Testing the text as is.\nTesting the text as is.Testing the text as is.Testing the text as is.Testing the text as isTesting the text as is. Testing the text as is.\nTesting the text as is.Testing the text as is.Testing the text as is.Testing the text as is."
   }
